7a6c1137eea468f8018a3376dc5e12b353bbafbd
Sumit Kumar <sumitk440@gmail.com>
2019-04-13 17:25:11 +0530

    excel file handle read data

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,9 +1,38 @@
+import java.io.File;
+import java.io.IOException;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
 
 public class Test 
 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws BiffException, IOException {
 		
-		System.out.println("heelo");
+		//.out.println("heelo");
+		
+		File filePath = new File("/Users/sumitkumar/Documents/ExcelDataRead.xls");
+		
+		Workbook wb=Workbook.getWorkbook(filePath);
+		
+		Sheet sb=wb.getSheet(0);
+		
+		int row =sb.getRows();
+		int col =sb.getColumns();
+		
+		for(int i=0; i<row; i++)
+		{
+			for (int j=0; j<col;j++)
+			{
+				Cell sheetCell= sb.getCell(j,i);
+				System.out.println(sheetCell.getContents());
+			}
+		}
+		
+		
+		
+		
 	}
 
 }
