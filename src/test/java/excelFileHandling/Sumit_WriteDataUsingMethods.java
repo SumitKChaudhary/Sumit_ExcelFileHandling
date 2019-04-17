7cee1d405d4e1a81487990b8920c0a8b8e2300c2
Sumit Kumar <sumitk440@gmail.com>
2019-04-17 17:20:54 +0530

    Create a method and pass the row_count and column_count and that method will write the data to that rows count and column count

--- src/test/java/excelFileHandling/Sumit_WriteDataUsingMethods.java
+++ src/test/java/excelFileHandling/Sumit_WriteDataUsingMethods.java
@@ -1,8 +1,52 @@
 package excelFileHandling;
 
+import java.io.File;
+import java.io.IOException;
 
+import jxl.Workbook;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+import jxl.write.biff.RowsExceededException;
+
+/*@author 	:	Sumit Kumar Chaudhary
+ * Class	:	Read the data from one sheet and copy to second sheet in one file 
+ * Date		:	16 April 2019 
+ * */
 public class Sumit_WriteDataUsingMethods 
 
 {
+	
+	public void createData(int row_No, int column_No ) throws IOException, RowsExceededException, WriteException
+	{
+		File createFile = new File("../ExcelFileHandlingbysumit/MethodData.xls");
+		
+		WritableWorkbook createWorkBook = Workbook.createWorkbook(createFile);
+		
+		WritableSheet createSheet = createWorkBook.createSheet("MethodData1", 0);
+		int a=0; 
+		for (int i=0; i<row_No; i++)
+		{
+			for (int j=0; j<column_No; j++)
+				
+			{
+				a++;
+				Label labelCell = new Label(j,i, "Sumit "+a);
+				createSheet.addCell(labelCell);
+			}
+		}
+		createWorkBook.write();
+		createWorkBook.close();
+		
+	}
+	
+	public static void main(String[] args) throws RowsExceededException, WriteException, IOException 
+	{
+		Sumit_WriteDataUsingMethods obj = new Sumit_WriteDataUsingMethods();
+		
+		obj.createData(3, 3);
+		System.out.println("Succeessfully Write");
+	}
 
 }
